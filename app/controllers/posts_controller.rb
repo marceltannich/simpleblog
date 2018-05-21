f54010e148d66350f5f4c26e19975de6a895845e
mardroid <marceltannich@gmail.com>
2018-05-21 18:43:20 +0200

    Add Blog Comments, Basic HTTP Auth

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+    http_basic_authenticate_with name: "admin", password:"1234", except: [:index, :show]
+
     def index
         @posts = Post.all
     end
@@ -8,7 +10,7 @@ class PostsController < ApplicationController
     end    
 
     def new
-
+        @post = Post.new
     end
 
     def create
@@ -20,6 +22,27 @@ class PostsController < ApplicationController
         else
             render 'new'
         end
+    end
+
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        @post = Post.find(params[:id])
+
+        if(@post.update(post_params))
+            redirect_to @post
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        
+        redirect_to posts_path
     end
 
     private def post_params
