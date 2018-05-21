46ce17ba909830d76c9cd977ae1d68c7c9c5c991
mardroid <marceltannich@gmail.com>
2018-05-21 13:40:58 +0200

    Add Posts, DB and Bootstrap

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,28 @@
 class PostsController < ApplicationController
     def index
+        @posts = Post.all
+    end
 
+    def show
+        @post = Post.find(params[:id])
+    end    
+
+    def new
+
+    end
+
+    def create
+        #render plain: params[:posts].inspect
+        @post = Post.new(post_params)
+
+        if(@post.save)
+            redirect_to @post
+        else
+            render 'new'
+        end
+    end
+
+    private def post_params
+        params.require(:post).permit(:title, :body)
     end
 end
